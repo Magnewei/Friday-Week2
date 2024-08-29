71afa2a56d1b7fb0d3f6773b29850288dc1d65df
Magnus N. <143735701+Magnewei@users.noreply.github.com>
2024-08-29 20:31:46 +0200

    Added lifecycle methods.

--- src/main/java/app/Persistence/Entities/Package.java
+++ src/main/java/app/Persistence/Entities/Package.java
@@ -1,6 +1,9 @@
 package app.Persistence.Entities;
+
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Builder
 @NoArgsConstructor
@@ -15,19 +18,33 @@ public class Package {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id",unique = true,nullable = false)
+    @Column(name = "id", unique = true, nullable = false)
     private long id;
 
-    @Column(name = "tracking_number",unique = false, nullable = false)
+    @Column(name = "tracking_number", unique = false, nullable = false)
     private String trackingNumber;
 
-    @Column(name = "sender_name",unique = false, nullable = false)
+    @Column(name = "sender_name", unique = false, nullable = false)
     private String senderName;
 
-    @Column(name = "receiver_name",unique = false, nullable = false)
+    @Column(name = "receiver_name", unique = false, nullable = false)
     private String receiverName;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "delivery_status",unique = false, nullable = false)
+    @Column(name = "delivery_status", unique = false, nullable = false)
     private DeliveryStatus deliveryStatus;
+
+
+    @Column(name = "last_updated", nullable = false)
+    private LocalDateTime lastUpdated;
+
+    @PrePersist
+    protected void onCreate() {
+        this.lastUpdated = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.lastUpdated = LocalDateTime.now();
+    }
 }
